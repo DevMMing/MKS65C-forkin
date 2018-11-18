b555aab1703472bf749a13e5b43c7bd4324e55a9
DevMMing <mming@stuy.edu>
2018-11-18 11:43:23 -0500

    finished hw

--- forkin.c
+++ forkin.c
@@ -16,17 +16,35 @@ int randNum(){
 int main(){
 	printf("out some initial message\n");
 	int child1=fork();
+	int sleeptime;
+	int deadtime;
 	if(child1==0){
-		printf("Hello, I am child1:%d\n. Let me take a quick nap.",getpid());
-		sleep(abs(randNum()%16));
-		printf("Ok. I am done sleeping.");
+		sleeptime=abs(randNum()%16)+5;
+		deadtime=sleeptime;
+		printf("Hello, I am child1:%d\n. Let me take a quick %d-second nap\n",getpid(),sleeptime);
+		sleep(sleeptime);
+		printf("Ok. I am done sleeping.\n");
+		return deadtime;
 	}else{
 	int child2=fork();
 	if(child2==0){
-		printf("Hello again. I am child2:%d\n. Let me take a quick nap",getpid());
-		sleep(abs(randNum()%16));
-		printf("Ok. I am done sleeping.");
+		sleeptime=abs(randNum()%16)+5;
+		deadtime=sleeptime;
+		printf("Hello again. I am child2:%d\n. Let me take a quick %d-second nap\n",getpid(),sleeptime);
+		sleep(sleeptime);
+		printf("Ok. I am done sleeping.\n");
+		return deadtime;
+	}
+	else{
+	int deadchild=wait(&deadtime);
+	if (WIFEXITED(deadtime)){
+        int ghost = WEXITSTATUS(deadtime);
+	  	printf("So sad poor jimmy had died! %d\n",deadchild);
+		printf("He only lived for %d seconds\n",ghost);
+		printf("Father got sad and went to sleep\n");
+		exit(0);
 	}
 	}
-	return 0;
+	}
+	
 }
